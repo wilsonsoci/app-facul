c3567ed477c9bde77f2b16edd37cbead657a70c3
wilsonsoci <75645210+wilsonsoci@users.noreply.github.com>
2024-10-14 22:59:57 -0300

    layout error fixed

---
+++ app/login.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import { useRouter } from 'expo-router';
+import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+
+export default function LoginScreen() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleLogin = () => {
+    if (username === 'admin' && password === '1234') {
+      Alert.alert('Login bem-sucedido!');
+      router.push('/'); // Voltar para a tela principal após o login
+    } else {
+      Alert.alert('Usuário ou senha incorretos!');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Usuário"
+        value={username}
+        onChangeText={setUsername}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 24,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+});
