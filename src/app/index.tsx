901b31e443f05c00fb724afebcab9681bc2683ba
wilsonsoci <75645210+wilsonsoci@users.noreply.github.com>
2024-10-16 18:41:12 -0300

    add: Fundo adicionado

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
 import { Button } from '../components/button';
 import { router } from 'expo-router';
 
@@ -6,30 +6,38 @@ import Constants from 'expo-constants';
 
 const statusBarHeight = Constants.statusBarHeight
 
+const backgroundImage = require('../../assets/images/background.jpg')
+
 export default function Index() {
  return (
    <View className='w-full' style={{ backgroundColor: "#3E2D3F", flex: 1 }}>
-    <View className='w-full flex flex-col items-center justify-center' style={{ marginTop: statusBarHeight + 8 }}>
-      <Text className='text-4xl text-white bold-text mt-40'>APP-FACUL</Text>
+    <ImageBackground
+      source={backgroundImage}
+      style={{flex: 1}}
+      resizeMode='cover'
+    >
+      <View className='w-full flex flex-col items-center justify-center' style={{ marginTop: statusBarHeight + 8 }}>
+        <Text className='text-4xl text-white bold-text mt-40'>APP-FACUL</Text>
 
-      <Text 
-        className='text-xl text-neutral-300 bold-text mt-4'>Bem-vindo ao app!</Text>
-    </View>
+        <Text 
+          className='text-xl text-neutral-300 bold-text mt-4'>Bem-vindo ao app!</Text>
+      </View>
 
-    <View className='w-full mt-32 flex flex-col items-center justify-center'>
-      <Button 
-        title='Criar conta' 
-        action={() => router.navigate("./tabs/auth/register")}
-      />
-      <Button 
-        title='Entrar' 
-        action={() => router.navigate("./tabs/auth/login")}
-      />
-    </View>
+      <View className='w-full mt-32 flex flex-col items-center justify-center'>
+        <Button 
+          title='Criar conta' 
+          action={() => router.navigate("./tabs/auth/register")}
+        />
+        <Button 
+          title='Entrar' 
+          action={() => router.navigate("./tabs/auth/login")}
+        />
+      </View>
 
-    <View className='w-full items-center justify-center mt-20'>
-      <Text className='text-neutral-600'>APP-FACUL™ 2024 </Text>
-    </View>
+      <View className='w-full items-center justify-center mt-20'>
+        <Text className='text-neutral-400'>APP-FACUL™ 2024 </Text>
+      </View>
+    </ImageBackground>
    </View>
   );
 }
